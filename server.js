907716046acf603cdd8123402a883fc61f762165
Justis <justis.d.mills@gmail.com>
2018-03-10 00:46:01 -0500

    console logging on client now, wow

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ dbClient.connect();
 dbClient.query('SELECT * FROM drafter', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
-    console.log(row.name);
     wss.clients.forEach((client) => {
       client.send(row.name + ":" + row.score);
     });
