3a4c96d6f5bc0075d84792812d3ac9349d79c5df
Justis <justis.d.mills@gmail.com>
2018-03-10 10:01:22 -0500

    experimenting with database writing

--- server.js
+++ server.js
@@ -18,18 +18,18 @@ const server = express()
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+// Database initialization with heroku postgres
+const { Client } = require('pg');
+
+const dbClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
-  // Database initialization with heroku postgres
-  const { Client } = require('pg');
-
-  const dbClient = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  });
 
   // Now that you're connected, connect to the database.
   dbClient.connect();
@@ -48,6 +48,36 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 
   ws.on('message', function incoming(data){
+    var splitData = data.split(":");
+    var name = splitData[0];
+    var score = splitData[1];
+
+    dbClient.connect();
+    // score update involves first and last name, so is contestant
+    if (splitData[0].split(" ").length > 1) {
+      // Have some scores!
+      dbClient.query('UPDATE player SET score = ' + score + ' WHERE name = ' + name, (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          wss.clients.forEach((client) => {
+            client.send(row.name + ":" + row.score);
+          });
+        }
+        dbClient.end();
+      });
+    } else {
+      // Have some player scores!
+      dbClient.query('UPDATE drafter SET score = ' + score + ' WHERE name = ' + name, (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          wss.clients.forEach((client) => {
+            client.send(row.name + ":" + row.score);
+          });
+        }
+        dbClient.end();
+      });
+    }
+
     wss.clients.forEach((client) => {
       client.send(data);
     });
